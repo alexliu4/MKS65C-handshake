6d1b72f6a0c1a57c0a620c27121895de9684bb0e
alexliu4 <aliu9@stuy.edu>
2018-12-19 15:24:30 -0500

    working on hw

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,7 +11,42 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  return 0;
+
+  int up;
+  char pid[HANDSHAKE_BUFFER_SIZE];
+  char msg[HANDSHAKE_BUFFER_SIZE];
+  char pipe1writemessage[30] = "Hello Server!!";
+
+
+  mkfifo("WKP", 0644);
+  printf("pipe created WKP\n");
+  up = open ("WKP", O_RDONLY);
+  //down = open ("server_side", O_RDWR); // kinda useless
+  printf("pipe opened: %d\n", up);
+  read(up, pid, sizeof(pid));
+  //semaphores can be used
+  remove("WKP");
+  *to_client = open(pid, O_WRONLY);
+  printf("Server PID given: %d\n", *to_client);
+
+  write(*to_client, pipe1writemessage, sizeof(pipe1writemessage));
+  read(*to_client, msg, sizeof(msg));
+  printf("MESSAGE: %s\n", msg);
+
+
+  // char pipe1writemessage[30] = "Hello Server!!";
+  // write(pid[1], pipe1writemessage, sizeof(pipe1writemessage));
+  //
+  // int down;
+  // mkfifo("client_side", 0644);
+  // printf("pipe created\n");
+  // down = open ("client_side", O_RDONLY);
+  // //down = open ("client_side", O_RDWR); // kinda useless
+  // printf("pipe opened: %d\n", down);
+  // //semaphores can be used
+
+
+  return *to_client;
 }
 
 
@@ -25,5 +60,25 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  return 0;
+  int up;
+  mkfifo("server_side", 0644);
+  printf("pipe created\n");
+  up = open ("server_side", O_RDONLY);
+  //down = open ("server_side", O_RDWR); // kinda useless
+  printf("pipe opened: %d\n", up);
+  //semaphores can be used
+
+  int down;
+  mkfifo("client_side", 0644);
+  printf("pipe created\n");
+  down = open ("client_side", O_RDONLY);
+  //down = open ("client_side", O_RDWR); // kinda useless
+  printf("pipe opened: %d\n", down);
+  //semaphores can be used
+  *to_server = down;
+
+  // close(to_child_fd[1]);  /* child closes write side of child  pipe */
+  // close(to_parent_fd[0]); /* child closes read  side of parent pipe */
+
+  return up;
 }
